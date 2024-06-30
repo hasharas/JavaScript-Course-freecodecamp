4cd290e6d2181cbe7f09873608750fe2f91c44e3
hasharas <hasharamsankalpam@gmail.com>
2024-07-01 01:03:01 +0530

    learn about operator

---
+++ operator.js
@@ -0,0 +1,12 @@
+// =,*,-
+//num=num+1 
+//num++
+//++num
+
+let num1=10;
+let num2=11;
+
+// const nuMequ = num1>num2;
+// const nuMequ = num1>=num2;
+const nuMequ = num1<num2;
+console.log(nuMequ);
