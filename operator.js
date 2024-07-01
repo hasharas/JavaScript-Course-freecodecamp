67c32e760c6db80ddf0e8bdb48e6b1bb91d75a36
hasharas <hasharamsankalpam@gmail.com>
2024-07-02 00:27:36 +0530

    memories t operators

--- operator.js
+++ operator.js
@@ -8,5 +8,13 @@ let num2=11;
 
 // const nuMequ = num1>num2;
 // const nuMequ = num1>=num2;
-const nuMequ = num1<num2;
-console.log(nuMequ);
+// const nuMequ = num1<num2;
+// console.log(nuMequ);
+
+// let age = 18;
+// const AgeType = age > 10 ? true : false;
+// console.log(AgeType);
+
+let Name = 100;
+const NameNew = Name > 110 ? 'correct' : 'wrong';
+console.log(NameNew);
