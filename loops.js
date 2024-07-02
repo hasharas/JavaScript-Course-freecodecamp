6e6280c8bba7a15e45d294ad595d41d79f36cc10
hasharas <hasharamsankalpam@gmail.com>
2024-07-03 02:32:48 +0530

    memorises for loops more

--- loops.js
+++ loops.js
@@ -15,17 +15,33 @@ let number = [1,2,3,4,5,6,7];
 //       console.log(ind);
 // }
 
+// let numbers = [1, 2, 4, 5, 6, 8, 7];
+// let evenNumbers = [];
+// let oddNumbers = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 === 0) {
+//         evenNumbers.push(numbers[i]);
+//     } else {
+//         oddNumbers.push(numbers[i]);
+//     }
+// }
+
+// console.log('Even numbers:', evenNumbers);
+// console.log('Odd numbers:', oddNumbers);
+
 let numbers = [1, 2, 4, 5, 6, 8, 7];
+
 let evenNumbers = [];
 let oddNumbers = [];
 
-for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 0) {
-        evenNumbers.push(numbers[i]);
+numbers.map(number => {
+    if (number % 2 === 0) {
+        evenNumbers.push(number);
     } else {
-        oddNumbers.push(numbers[i]);
+        oddNumbers.push(number);
     }
-}
+});
 
 console.log('Even numbers:', evenNumbers);
 console.log('Odd numbers:', oddNumbers);
