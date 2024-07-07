891cf6d8af8d369c28128ec51331aebcdb84a425
hasharas <hasharamsankalpam@gmail.com>
2024-07-08 00:22:56 +0530

    try for loop diferent way

---
+++ for.js
@@ -0,0 +1,10 @@
+  
+//   for (let ind = 0; ind < 100; ind +=2){
+//       console.log(ind);
+//   }
+
+let NUmberN = [1,2,3,4,5,6,7,8];
+
+for (let indx=1; indx<NUmberN.length; indx+=2){
+      console.log(NUmberN[indx]);
+}
