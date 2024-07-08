68e2c5b8df826a67b5cb14d302786bd001f817a1
hasharas <hasharamsankalpam@gmail.com>
2024-07-08 20:14:23 +0530

    while prac time

--- for.js
+++ for.js
@@ -3,8 +3,26 @@
 //       console.log(ind);
 //   }
 
-let NUmberN = [1,2,3,4,5,6,7,8];
+// let NUmberN = [1,2,3,4,5,6,7,8];
 
-for (let indx=1; indx<NUmberN.length; indx+=2){
-      console.log(NUmberN[indx]);
+// for (let indx=1; indx<NUmberN.length; indx+=2){
+//       console.log(NUmberN[indx]);
+// }
+
+// let namee= [1,2,3,4,5,6,7];
+// let ind = 1;
+
+// while(ind < namee.length){
+//       console.log(namee[ind]);
+//       ind+=2;
+// }
+
+let sum = 0;
+
+while(true){
+      console.log('looop ...');
+      sum++;
+      if(sum === 10)
+            break;
+      
 }
